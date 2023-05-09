d52d3a7f99a5d1566695aa5dd7d87a8d517d4841
Ruslan Kriklivyy <ruslan@quartsoft.com>
2023-05-09 11:08:37 +0300

    update: env vars and packages

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
+import {API_URL, UNSPLASH_CLIENT_ID} from '@env';
 
 export const instance = axios.create({
-  baseURL: 'https://api.unsplash.com',
+  baseURL: API_URL,
   headers: {
-    Authorization:
-      'Client-ID 896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043',
+    Authorization: `Client-ID ${UNSPLASH_CLIENT_ID}`,
   },
 });
